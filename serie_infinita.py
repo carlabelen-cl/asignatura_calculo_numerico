1ef1b758bd7c854560f21e2dec5bfd764303c718
carlabelen-cl <carlav025qgmail.com>
2026-09-03 15:32:13 -0400

    error de la sumatoria inversa

--- serie_infinita.py
+++ serie_infinita.py
@@ -27,3 +27,11 @@ for a in range(n, 0,-1):
     sumatoria_inversa = sum(1 / (j ** 4) for j in range(1, a + 1))
     serie_infinita_inversa.append(sumatoria_inversa) 
 print("La sumatoria inversa de 1/i^4 hasta", n, "términos es:", serie_infinita_inversa[0])
+
+Ea_inverso= abs(constante - serie_infinita_inversa[0])
+Er_inverso= Ea_inverso / constante
+Ep_inverso= Er_inverso * 100 
+
+print(f"El error porcentual para {n} términos es (inverso): {Ep_inverso}%")
+print(f"El valor de la sumatoria es (inverso): {serie_infinita_inversa[0]} y el valor de la constante es: {constante}")
+print(f"El error absoluto es (inverso): {Ea_inverso} y el error relativo es (inverso): {Er_inverso}")
